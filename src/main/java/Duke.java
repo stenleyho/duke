d50212af196d3adb653fbb8056cd256ddecee3f5
stenleyho <stenleyho@hotmail.com>
2019-09-03 18:43:36 +0800

    no message

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,19 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
-public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-    }
-}
+//public class Duke {
+//    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+//    }
+//}
 
-public class Main {
+public class Duke {
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
@@ -21,17 +22,37 @@ public class Main {
 
     public static void GetInPut() {
         String line;
+        int count = 0;
+        String[] data = new String[100];
         Scanner input = new Scanner(System.in);
         line = input.toString();
+
 
         while (!line.equalsIgnoreCase("bye")) {
             line = input.nextLine();
             if (line.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(line);
+            if (line.equalsIgnoreCase("list")) {
+                GetList(data, count);
+            }
+            else {
+                StoreList(data, line, count);
+                count++;
+            }
         }
-        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static String[] StoreList(String[] data, String sentence, int pos) {
+        data[pos] = sentence;
+        System.out.println("added:" + sentence);
+        return data;
+    }
+
+    public static void GetList(String[] data, int pos) {
+        for (int i=0; i<pos; i++) {
+            System.out.println(i + 1 + "." + data[i]);
+        }
     }
 }
-
