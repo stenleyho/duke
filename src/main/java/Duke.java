e56130914cb3c5d02d73f3ae650f186d22a34896
stenleyho <stenleyho@hotmail.com>
2019-09-03 15:21:36 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,3 +11,27 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
     }
 }
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        GetInPut();
+    }
+
+    public static void GetInPut() {
+        String line;
+        Scanner input = new Scanner(System.in);
+        line = input.toString();
+
+        while (!line.equalsIgnoreCase("bye")) {
+            line = input.nextLine();
+            if (line.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.println(line);
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
+
