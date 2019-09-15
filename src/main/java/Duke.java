68ed1aa1d679793a08b0e2264a100d9075f8ebbb
stenleyho <stenleyho@hotmail.com>
2019-09-15 21:48:57 +0800

    level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -18,36 +18,69 @@ public class Duke {
     public static void GetInPut() {
         String line;
         int count = 0;
-        String[] data = new String[100];
+//        String[] data = new String[100];
         Scanner input = new Scanner(System.in);
         line = input.toString();
+        Task[] t = new Task[100];
 
-
+//    while(true) {
+//        switch (line.toUpperCase()) {
+//            case "LIST":
+//                GetList(t, count);
+//                break;
+//            case "BYE":
+//                System.out.println("Bye. Hope to see you again soon!");
+//                break;
+//            case "DONE":
+//                int line_num = Integer.parseInt(line.split(" ")[1]);
+////                System.out.println("im here");
+//                for (int i = 0; line_num == i; i++) {
+//                    t[line_num].markAsDone();
+//                    System.out.println("this is" + line_num);
+//                    break;
+//                }
+//            default:
+//                    t[count] = new Task(line);
+//                    StoreList(t, line, count);
+//                    count++;
+//                    break;
+//                }
+//        }
+//    }
         while (!line.equalsIgnoreCase("bye")) {
             line = input.nextLine();
+
             if (line.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            if (line.equalsIgnoreCase("list")) {
-                GetList(data, count);
+            if (line.split(" ")[0].equalsIgnoreCase("done")) {
+                int line_num = Integer.parseInt(line.split(" ")[1]);
+//                System.out.println("this is " + line_num);
+                    t[line_num-1].markAsDone();
             }
-            else {
-                StoreList(data, line, count);
+            if (line.equalsIgnoreCase("list")) {
+                GetList(t, count);
+            }
+            else if(!line.equalsIgnoreCase("list")&&
+                    !line.split(" ")[0].equalsIgnoreCase("done")) {
+                t[count] = new Task (line);
+                StoreList(t, line, count);
                 count++;
             }
         }
     }
 
-    public static String[] StoreList(String[] data, String sentence, int pos) {
-        data[pos] = sentence;
-        System.out.println("added:" + sentence);
-        return data;
+    public static Task[] StoreList(Task[] t, String line, int count) {
+        t[count] = new Task(line);
+        System.out.println("added:" + line);
+        return t;
     }
 
-    public static void GetList(String[] data, int pos) {
-        for (int i=0; i<pos; i++) {
-            System.out.println(i + 1 + "." + data[i]);
+    public static void GetList(Task[] t, int count) {
+        for (int i=0; i<count; i++) {
+            String icon = t[i].getStatusIcon();
+            System.out.println(i + 1 + "." + "["+ icon +"]" + t[i].getDescription());
         }
     }
 }
---
+++ src/main/java/Task.java
@@ -0,0 +1,27 @@
+//import java.Duke;
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task (String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+//    public String toString(){
+//        return
+//    }
+}
