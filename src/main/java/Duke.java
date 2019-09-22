768de131b4ddac7345d6b1f4cc23187ba629a6bf
stenleyho <stenleyho@hotmail.com>
2019-09-22 22:30:47 +0800

    level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,32 @@
-import java.util.Scanner;
-import java.util.Arrays;
+import java.util.*;
 
 public class Duke {
+    private static Task[] tasks = new Task[100];
+    private static int tCount = 0;
+
+    public static void addTask(Task t){
+        tasks[tCount] = t;
+        System.out.println("-----------------------------------------------");
+        System.out.println("added:" + t.getDescription());
+        System.out.println("-----------------------------------------------");
+        tCount++;
+    }
+
+    public static void GetList() {
+        System.out.println("-----------------------------------------------");
+            for (int i=0; i<tCount; i++) {
+                System.out.println(i + 1 + ". " + tasks[i]);
+            }
+        System.out.println("-----------------------------------------------");
+    }
+
+    public static void markAsDone(int pos) {
+         tasks[pos-1].markAsDone(true);
+        System.out.println("-----------------------------------------------");
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("    [" +  tasks[pos-1].getStatusIcon() + "]" +  tasks[pos-1].getDescription());
+        System.out.println("-----------------------------------------------");
+    }
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -12,75 +37,72 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        GetInPut();
-    }
-
-    public static void GetInPut() {
         String line;
-        int count = 0;
-//        String[] data = new String[100];
-        Scanner input = new Scanner(System.in);
-        line = input.toString();
-        Task[] t = new Task[100];
-
-//    while(true) {
-//        switch (line.toUpperCase()) {
-//            case "LIST":
-//                GetList(t, count);
-//                break;
-//            case "BYE":
-//                System.out.println("Bye. Hope to see you again soon!");
-//                break;
-//            case "DONE":
-//                int line_num = Integer.parseInt(line.split(" ")[1]);
-////                System.out.println("im here");
-//                for (int i = 0; line_num == i; i++) {
-//                    t[line_num].markAsDone();
-//                    System.out.println("this is" + line_num);
-//                    break;
-//                }
-//            default:
-//                    t[count] = new Task(line);
-//                    StoreList(t, line, count);
-//                    count++;
-//                    break;
-//                }
-//        }
-//    }
-        while (!line.equalsIgnoreCase("bye")) {
-            line = input.nextLine();
-
-            if (line.equalsIgnoreCase("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            }
-            if (line.split(" ")[0].equalsIgnoreCase("done")) {
-                int line_num = Integer.parseInt(line.split(" ")[1]);
-//                System.out.println("this is " + line_num);
-                    t[line_num-1].markAsDone();
-            }
-            if (line.equalsIgnoreCase("list")) {
-                GetList(t, count);
-            }
-            else if(!line.equalsIgnoreCase("list")&&
-                    !line.split(" ")[0].equalsIgnoreCase("done")) {
-                t[count] = new Task (line);
-                StoreList(t, line, count);
-                count++;
+        String [] linePart;
+        while(true) {
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            linePart = line.split(" ", 2);
+            switch (line.toUpperCase()) {
+                case "LIST":
+                    GetList();
+                    break;
+                case "BYE":
+                    System.out.println("Bye. Hope to see you again soon!");
+                    return;
+                default:
+                    switch (linePart[0].toUpperCase())
+                    {
+                        case "DONE":
+			                markAsDone(Integer.parseInt(linePart[1]));
+                            break;
+                        case "TODO":
+                            addTask(new Todo(linePart[1]));
+                            break;
+                        case "DEADLINE":
+                            String DeadlineArr[] = linePart[1].split("/by", 2);
+                            addTask(new Deadlines(DeadlineArr[0],DeadlineArr[1])); ;
+                            break;
+                        case "EVENT":
+                            String EventArr[] = linePart[1].split("/at", 2);
+                            addTask(new Events(EventArr[0],EventArr[1])); ;
+                            break;
+                        default:
+                            System.out.println("Please type 'Todo' or 'Deadline /by' or 'Event /at' to add task. Type 'List' to see tasks and 'Bye' to exit");
+//                            addTask(new Todo(line));
+                    }
+                    break;
             }
         }
     }
 
-    public static Task[] StoreList(Task[] t, String line, int count) {
-        t[count] = new Task(line);
-        System.out.println("added:" + line);
-        return t;
+    public static void addTask(Todo t){
+        tasks[tCount] = t;
+        System.out.println("-----------------------------------------------");
+        System.out.println("Got it. I've added this task: :");
+        System.out.println(t);
+        tCount++;
+        System.out.println("Now you have " + tCount + " tasks in the list");
+        System.out.println("-----------------------------------------------");
     }
 
-    public static void GetList(Task[] t, int count) {
-        for (int i=0; i<count; i++) {
-            String icon = t[i].getStatusIcon();
-            System.out.println(i + 1 + "." + "["+ icon +"]" + t[i].getDescription());
-        }
+    public static void addTask(Deadlines t){
+        tasks[tCount] = t;
+        System.out.println("-----------------------------------------------");
+        System.out.println("Got it. I've added this task: :");
+        System.out.println(t);
+        tCount++;
+        System.out.println("Now you have " + tCount + " tasks in the list");
+        System.out.println("-----------------------------------------------");
+    }
+
+    public static void addTask(Events t){
+        tasks[tCount] = t;
+        System.out.println("-----------------------------------------------");
+        System.out.println("Got it. I've added this task: :");
+        System.out.println(t);
+        tCount++;
+        System.out.println("Now you have " + tCount + " tasks in the list");
+        System.out.println("-----------------------------------------------");
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,27 +1,29 @@
-//import java.Duke;
-
 public class Task {
     protected String description;
     protected boolean isDone;
 
-    public Task (String description) {
+    public Task(String description) {
         this.description = description;
         this.isDone = false;
+    }
+
+    public void markAsDone(boolean isDone)
+    {
+        this.isDone = isDone;
     }
 
     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
-    public String getDescription() {
+    public String getDescription()
+    {
         return description;
     }
 
-    public void markAsDone() {
-        this.isDone = true;
+    @Override
+    public String toString(){
+        return "[" + this.getStatusIcon() + "]" + this.getDescription();
     }
 
-//    public String toString(){
-//        return
-//    }
 }
